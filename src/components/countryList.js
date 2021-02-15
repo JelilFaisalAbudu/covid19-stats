17c99c5735fac639d8d27a8b4c988bb502cec844
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-02-15 13:59:08 +0000

    Add prop types validation to the CountryList component

--- src/components/countryList.js
+++ src/components/countryList.js
@@ -1,22 +1,30 @@
-/* eslint-disable */
+import PropTypes from 'prop-types';
 
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import React from 'react';
 import Country from './Country';
 
 const CountryList = ({ countries }) => (
-  <div className="row" style={{
-    display: 'flex',
-    justifyContent: 'space-evenly',
-  }}>
+  <div
+    className="row"
+    style={{
+      display: 'flex',
+      justifyContent: 'space-evenly',
+    }}
+  >
     {countries.map(country => (
-    <Country key={country.ID} country={country}>
-      <Link to={`/countries/${country.Country}`}>
-        View Details
-      </Link>
-    </Country>
-  ))}
+      <Country key={country.ID} country={country}>
+        <Link to={`/countries/${country.Country}`}>
+          View Details
+        </Link>
+      </Country>
+    ))}
   </div>
 );
+
+CountryList.propTypes = {
+  countries: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 export default CountryList;
