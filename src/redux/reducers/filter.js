cb0986b5931062fbfd53d5b833d58ccd47dbb50e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-02-15 06:10:46 +0000

    Fix linter errors

--- src/redux/reducers/filter.js
+++ src/redux/reducers/filter.js
@@ -1,13 +1,13 @@
-import { SET_FILTER } from '../actionTypes';
+// import { SET_FILTER } from '../actionTypes';
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case SET_FILTER:
-      return action.payload;
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case SET_FILTER:
+//       return action.payload;
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-export default filterReducer;
+// export default filterReducer;
